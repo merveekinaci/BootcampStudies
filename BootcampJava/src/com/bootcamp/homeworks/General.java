a251c21c07bf16ec9d8dede0871454782e566761
ahmetyavuzoruc <ahmetyavuzoruc@gmail.com>
2021-09-16 09:21:31 +0300

    new homework

---
+++ BootcampJava/src/com/bootcamp/homeworks/General.java
@@ -0,0 +1,15 @@
+package com.bootcamp.homeworks;
+
+public class General {
+
+  /**
+   *      1- Bir array oluşturun ve array içindeki en büyük sayıyı bulan bir algoritma tasarlayın
+   *      2- Aynı array içinde ikinci en büyük sayıyı bulan algoritma tasarlayın
+   *      3- Aynı array içindeki çift sayıları başka bir arraya ve tek sayıları başka bir arraya setleyin, ve yeni oluşan 2 arrayi de ekrana yazdırın.
+   *      4- Aynı arrayi ters şekilde başka bir arraya setleyin.
+   *
+   *      Bu 4 farklı algoritmayı tasarlayın ve github'ınıza pushlayın.
+   *
+   */
+
+}
